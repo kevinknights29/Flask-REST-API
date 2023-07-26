07378b06325f2c464fd1593acc5291db9497fce3
Kevin Knights <kevinknights29@hotmail.com>
2023-07-26 12:16:28 -0500

    WIP: addded basic functionality

---
+++ app.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Wonderful Store",
+        "items": [
+            {
+                "name": "My Item",
+                "price": 15.99,
+            },
+        ],
+    },
+]
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
