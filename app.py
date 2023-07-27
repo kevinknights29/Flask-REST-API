a570ede23c3f2ba2a67b2e4aaad87b8fbc45094b
Kevin Knights <kevinknights29@hotmail.com>
2023-07-27 06:32:51 -0500

    WIP: added create item POST method

--- app.py
+++ app.py
@@ -30,3 +30,14 @@ def create_store():
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return {"store": stores[-1]}, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return {"item": new_item}, 201
+        return {"message": "store not found"}, 404
