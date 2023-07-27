470915efec1259bea269d04e70d3134a2c85fed1
Kevin Knights <kevinknights29@hotmail.com>
2023-07-27 06:08:14 -0500

    WIP: added create store POST method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from flask import Flask
+from flask import request
 
 
 app = Flask(__name__)
@@ -21,3 +22,11 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return {"store": stores[-1]}, 201
